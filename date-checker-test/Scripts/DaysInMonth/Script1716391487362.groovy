8232a7d83fd7fc7bd49c4ffdbcbebf82b94ef094
hukoFpt <lynnie.nguyen.bns@gmail.com>
2024-06-12 20:35:27 +0700

    chore: Refactor date validation logic and update input fields in page.tsx

--- date-checker-test/Scripts/DaysInMonth/Script1716391487362.groovy
+++ date-checker-test/Scripts/DaysInMonth/Script1716391487362.groovy
@@ -1,7 +1,29 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
 WebUI.openBrowser('')
 WebUI.navigateToUrl('http://localhost:3000/')
-def result = WebUI.executeJavaScript("return DaysInMonth(2, 2020);", null)
+
+// Test for months with 31 days
+def monthsWith31Days = [1, 3, 5, 7, 8, 10, 12]
+monthsWith31Days.each {
+    def result = WebUI.executeJavaScript("return DaysInMonth($it, 2022);", null)
+    WebUI.verifyMatch(result.toString(), '31', false)	
+}
+
+// Test for months with 30 days
+def monthsWith30Days = [4, 6, 9, 11]
+monthsWith30Days.each {
+    def result = WebUI.executeJavaScript("return DaysInMonth($it, 2022);", null)
+    WebUI.verifyMatch(result.toString(), '30', false)
+}
+
+// Test for February in a common year
+def result = WebUI.executeJavaScript("return DaysInMonth(2, 2022);", null)
+WebUI.verifyMatch(result.toString(), '28', false)
+
+// Test for February in a leap year
+result = WebUI.executeJavaScript("return DaysInMonth(2, 2020);", null)
 WebUI.verifyMatch(result.toString(), '29', false)
+
 WebUI.closeBrowser()
